35391c98d2c97935e927566825e27bf4effc0bab
S.A.V.I.O.U.R <S.A.V.I.O.U.R@SIMILLUSTRA.COM>
2022-04-28 14:47:29 +0100

    SSL Added

--- scripts/install_composer_dependencies.sh
+++ scripts/install_composer_dependencies.sh
@@ -7,9 +7,32 @@ curl -sS https://getcomposer.org/installer -o /tmp/composer-setup.php
 sudo php /tmp/composer-setup.php --install-dir=/usr/local/bin --filename=composer
 
 cd /var/www/html
-sudo \cp siteConfig/000-default.conf /etc/apache2/sites-available/
+#Updating Write permission
+sudo chown -R ubuntu /var/opt
+
+   if [ ! -d  /var/opt/ssl ]; then
+    sudo mkdir /var/opt/ssl
+    echo "SSL Directory Created"
+  fi
+
+sudo \cp -rf siteConfig/psg.ng.crt /var/opt/ssl/
+sudo \cp -rf siteConfig/psg.ng.key /var/opt/ssl/
+
+sudo chown -R root.root /var/opt
+
+sudo \cp -rf siteConfig/000-default.conf /etc/apache2/sites-available/
+sudo \cp -rf siteConfig/default-ssl.conf /etc/apache2/sites-available/
+sudo \cp -rf siteConfig/ssl-params.conf /etc/apache2/conf-available/
+
+sudo ln -s /etc/apache2/sites-available/default-ssl.conf /etc/apache2/sites-enabled/default-ssl.conf
+
 curl -sS https://getcomposer.org/installer | php
 php composer.phar install
+
+sudo a2enmod ssl
+sudo a2enmod headers
+sudo a2enconf ssl-params
+sudo a2ensite default-ssl
 
 sudo systemctl reload apache2
 
